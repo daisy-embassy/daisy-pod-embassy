a369b81f6b8ceb26dc442d1a848b916ac5643ea6
Dickless <011001nobuo@gmail.com>
2026-02-25 01:04:27 +0900

    add feature configuration checks for target board selection

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
 #![no_std]
 
+// use same configuration concept as https://github.com/zlosynth/daisy
+#[cfg(all(
+    feature = "seed_1_2",
+    any(feature = "seed_1_1", feature = "seed", feature = "patch_sm")
+))]
+compile_error!("only a single target board must be selected");
+
+#[cfg(all(
+    feature = "seed_1_1",
+    any(feature = "seed_1_2", feature = "seed", feature = "patch_sm")
+))]
+compile_error!("only a single target board must be selected");
+
+#[cfg(all(
+    feature = "seed",
+    any(feature = "seed_1_2", feature = "seed_1_1", feature = "patch_sm")
+))]
+compile_error!("only a single target board must be selected");
+
+#[cfg(all(
+    feature = "patch_sm",
+    any(feature = "seed_1_2", feature = "seed_1_1", feature = "seed")
+))]
+compile_error!("only a single target board must be selected");
+
+#[cfg(not(any(
+    feature = "seed_1_2",
+    feature = "seed_1_1",
+    feature = "seed",
+    feature = "patch_sm"
+)))]
+compile_error!(
+    "target board must be selected using a feature: \"seed_1_2\" | \"seed_1_1\" | \"seed\" | \"patch_sm\""
+);
+
 pub use daisy_embassy;
