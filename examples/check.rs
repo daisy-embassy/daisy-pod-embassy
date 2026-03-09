7f0ec76a307fe6c0f95d2009d096d68ff990b274
Dickless <011001nobuo@gmail.com>
2026-03-10 00:53:58 +0900

    add blinky task

--- examples/check.rs
+++ examples/check.rs
@@ -7,11 +7,13 @@ use daisy_embassy::hal::bind_interrupts;
 use daisy_embassy::hal::gpio::Pull;
 use daisy_embassy::hal::mode::Async;
 use daisy_embassy::hal::{self, exti::ExtiInput};
+use daisy_embassy::led::UserLed;
 use daisy_embassy::new_daisy_board;
 use daisy_pod_embassy::peri::DaisyPodPeripherals;
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+use embassy_time::{Duration, Ticker};
+use grounded::uninit::GroundedArrayCell;
 
 use {defmt_rtt as _, panic_probe as _};
 bind_interrupts!(
@@ -27,19 +29,29 @@ async fn main(spawner: Spawner) {
     info!("Hello World!");
     let daisy_p = new_daisy_board!(p);
     let pod_p = DaisyPodPeripherals::new(daisy_p, p.ADC1, p.ADC2, p.USART1, p.USB_OTG_HS);
+
+    spawner.must_spawn(blinky(pod_p.user_led));
+
     let button = ExtiInput::new(pod_p.tact_switches.tac_switch_1, p.EXTI9, Pull::Down, Irqs);
     spawner.must_spawn(tac_switch_task(button, "tac_1"));
     let button = ExtiInput::new(pod_p.tact_switches.tac_switch_2, p.EXTI2, Pull::Down, Irqs);
     spawner.must_spawn(tac_switch_task(button, "tac_2"));
-    let mut led = pod_p.user_led;
+}
 
+#[embassy_executor::task]
+async fn blinky(mut pin: UserLed<'static>) {
+    let mut ticker = Ticker::every(Duration::from_millis(300));
     loop {
-        info!("on");
-        led.on();
-        Timer::after_millis(300).await;
-
-        info!("off");
-        led.off();
-        Timer::after_millis(300).await;
+        pin.off();
+        ticker.next().await;
+        pin.on();
+        ticker.next().await;
+    }
+}
+#[embassy_executor::task(pool_size = 2)]
+async fn tac_switch_task(mut pin: ExtiInput<'static, Async>, identifier: &'static str) -> ! {
+    loop {
+        pin.wait_for_low().await;
+        info!("{} pressed", identifier);
     }
 }
