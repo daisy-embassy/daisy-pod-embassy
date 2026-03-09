1e41977fe0b2704a7769b327a2f5f15d55cc11c8
Dickless <011001nobuo@gmail.com>
2026-03-10 00:47:55 +0900

    Refactor check example to include print  tasks for tact switches

--- examples/check.rs
+++ examples/check.rs
@@ -3,7 +3,10 @@
 
 //! A test example to verify pin and peripheral functionality
 
-use daisy_embassy::hal;
+use daisy_embassy::hal::bind_interrupts;
+use daisy_embassy::hal::gpio::Pull;
+use daisy_embassy::hal::mode::Async;
+use daisy_embassy::hal::{self, exti::ExtiInput};
 use daisy_embassy::new_daisy_board;
 use daisy_pod_embassy::peri::DaisyPodPeripherals;
 use defmt::info;
@@ -11,14 +14,23 @@ use embassy_executor::Spawner;
 use embassy_time::Timer;
 
 use {defmt_rtt as _, panic_probe as _};
+bind_interrupts!(
+    pub struct Irqs{
+        EXTI9_5 => hal::exti::InterruptHandler<hal::interrupt::typelevel::EXTI9_5>;
+        EXTI2 => hal::exti::InterruptHandler<hal::interrupt::typelevel::EXTI2>;
+});
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let rcc = daisy_embassy::default_rcc();
     let p = hal::init(rcc);
     info!("Hello World!");
     let daisy_p = new_daisy_board!(p);
     let pod_p = DaisyPodPeripherals::new(daisy_p, p.ADC1, p.ADC2, p.USART1, p.USB_OTG_HS);
+    let button = ExtiInput::new(pod_p.tact_switches.tac_switch_1, p.EXTI9, Pull::Down, Irqs);
+    spawner.must_spawn(tac_switch_task(button, "tac_1"));
+    let button = ExtiInput::new(pod_p.tact_switches.tac_switch_2, p.EXTI2, Pull::Down, Irqs);
+    spawner.must_spawn(tac_switch_task(button, "tac_2"));
     let mut led = pod_p.user_led;
 
     loop {
